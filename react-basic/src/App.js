295b511d2a0f03724a65fa96d5bf1cea74027f83
yshi80 <yshi80@ford.com>
2024-09-25 17:05:03 +0800

    实现复杂条件渲染

--- react-basic/src/App.js
+++ react-basic/src/App.js
@@ -1,10 +1,25 @@
 // 项目的根组件
 // App -> index.js -> public/index.html(root)
 
+// 定义文章类型
+const articleType = 3; // 0 1 3
+
+// 定义核心函数（根据文章类型返回不同的 JSX 模板）
+function getArticleTem() {
+    if (articleType === 0) {
+        return <div>无图模式</div>
+    } else if (articleType === 1) {
+        return <div>单图模式</div>
+    } else {
+        return <div>三图模式</div>
+    }
+}
+
 function App() {
     return (
         <div className="App">
-            this is App
+            {/*{调用函数渲染不同的模板}*/}
+            {getArticleTem()}
         </div>
     );
 }
