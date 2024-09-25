6a2239fa1451b86a7126560444ec7968b49811cb
yshi80 <yshi80@ford.com>
2024-09-25 15:44:37 +0800

    实现基础条件渲染

--- react-basic/src/App.js
+++ react-basic/src/App.js
@@ -1,10 +1,16 @@
 // 项目的根组件
 // App -> index.js -> public/index.html(root)
 
+const isLogin = false;
+
 function App() {
     return (
         <div className="App">
-            this is App
+            {/*{逻辑与 &&}*/}
+            {isLogin && <span>this is span</span>}
+
+            {/*{三元运算}*/}
+            {isLogin ? <span>React</span> : <span>Loading...</span>}
         </div>
     );
 }
