ae0b07d65e4118a76c74b7e45347c3f8d14e5345
yshi80 <yshi80@ford.com>
2024-09-28 00:52:04 +0800

    React中的事件绑定

--- react-basic/src/App.js
+++ react-basic/src/App.js
@@ -2,9 +2,29 @@
 // App -> index.js -> public/index.html(root)
 
 function App() {
+    // 基础绑定
+    // const handleClick = () => {
+    //     console.log("on button click");
+    // }
+
+    // 事件参数 e
+    // const handleClick = (e) => {
+    //     console.log("on button click", e);
+    // }
+
+    // 传递自定义参数
+    // const handleClick = (name) => {
+    //     console.log("on button click", name);
+    // }
+
+    // 传递自定义参数和事件对象 e
+    const handleClick = (name, e) => {
+        console.log("on button click", name, e);
+    }
+
     return (
         <div className="App">
-            this is App
+            <button onClick={(e) => handleClick("React", e)}>click me</button>
         </div>
     );
 }
