5645b7cf166a02871ed3d74ad01228ddfec5b649
yshi80 <yshi80@ford.com>
2024-09-28 01:11:01 +0800

    React组件基础使用

--- react-basic/src/App.js
+++ react-basic/src/App.js
@@ -1,10 +1,19 @@
-// 项目的根组件
-// App -> index.js -> public/index.html(root)
+// 1、定义组件
+
+function Button() {
+    // 业务逻辑组件逻辑
+    return <button>click me</button>;
+}
 
 function App() {
     return (
         <div className="App">
-            this is App
+            {/*{2、使用组件(渲染组件)}*/}
+            {/*{自闭和}*/}
+            <Button/>
+
+            {/*{成对标签}*/}
+            <Button></Button>
         </div>
     );
 }
