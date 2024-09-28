683c66f396487f0a236f6898928b6a8bd6d5b170
yshi80 <yshi80@ford.com>
2024-09-28 10:36:45 +0800

    useState基础使用

--- react-basic/src/App.js
+++ react-basic/src/App.js
@@ -1,10 +1,22 @@
-// 项目的根组件
-// App -> index.js -> public/index.html(root)
+// useState 实现一个计数器按钮
+import {useState} from "react";
 
 function App() {
+    // 1. 调用 useState 添加一个状态变量
+    // count 状态变量
+    // setCount 修改状态变量的方法
+    const [count, setCount] = useState(0);
+
+    // 2. 点击事件回调
+    const handleClick = () => {
+        // 作用：1. 用传入的新值修改 count
+        // 2. 重新使用新的 count 渲染 UI
+        setCount(count + 1);
+    }
+
     return (
         <div className="App">
-            this is App
+            <button onClick={handleClick}>{count}</button>
         </div>
     );
 }
