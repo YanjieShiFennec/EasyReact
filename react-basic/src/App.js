67a04acdc665e9f176ca71dbb1a8d065b7877efc
yshi80 <yshi80@ford.com>
2024-09-29 19:12:26 +0800

    表单受控绑定

--- react-basic/src/App.js
+++ react-basic/src/App.js
@@ -1,10 +1,22 @@
-// 项目的根组件
-// App -> index.js -> public/index.html(root)
+// 受控表单绑定
+
+import {useState} from 'react'
+
+// 1. 声明一个 react 状态 - useState
+
+// 2. 核心绑定流程
+// 1. 通过 value 属性绑定 react 状态
+// 2. 绑定 onChange 事件 通过事件参数 e 拿到输入框最新的值 反向修改到 react 状态
 
 function App() {
+    const [value, setValue] = useState('')
+
     return (
         <div className="App">
-            this is App
+            <input
+                value={value}
+                onChange={(e) => setValue(e.target.value)}
+                type="text"/>
         </div>
     );
 }
