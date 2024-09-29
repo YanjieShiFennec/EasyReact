1e0feffee40bafa98c0f5f8cb4c3a77e221a8161
yshi80 <yshi80@ford.com>
2024-09-29 21:58:18 +0800

    组件通信-父传子-props说明

--- react-basic/src/App.js
+++ react-basic/src/App.js
@@ -5,14 +5,22 @@
 function Son(props) {
     // props: 对象里面包含了父组件传递过来的所有数据
     console.log(props);
-    return <div>this is son {props.name}</div>
+    return <div>this is son, {props.name}, jsx: {props.child}</div>
 }
 
 function App() {
     const name = "this is app name";
     return (
         <div className="App">
-            <Son name={name}/>
+            <Son
+                name={name}
+                age={18}
+                isTrue={false}
+                list={['vue', 'react']}
+                obj={{name:'react'}}
+                cb={()=>console.log(123)}
+                child={<span>this is span</span>}
+            />
         </div>
     );
 }
