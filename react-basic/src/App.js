72dd4278a7795740f59a1b56aaec239950cebcac
yshi80 <yshi80@ford.com>
2024-09-29 19:19:17 +0800

    React中获取DOM

--- react-basic/src/App.js
+++ react-basic/src/App.js
@@ -1,10 +1,22 @@
-// 项目的根组件
-// App -> index.js -> public/index.html(root)
+// React 中获取 DOM
+
+// 1. useRef 生成 ref 对象 绑定到 dom 标签上
+
+// 2. dom 可用时， ref.current 获取 dom
+// 渲染完成之后 dom 生成之后才可用
+
+import {useRef} from "react";
 
 function App() {
+    const inputRef = useRef();
+    const showDom = ()=>{
+        console.dir(inputRef.current);
+    };
+
     return (
         <div className="App">
-            this is App
+            <input type={"text"} ref={inputRef}/>
+            <button onClick={showDom}>获取 dom</button>
         </div>
     );
 }
