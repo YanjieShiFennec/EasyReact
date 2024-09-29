a8f03527e814e1d48b900a685cf1c6ba505f3f68
yshi80 <yshi80@ford.com>
2024-09-29 20:00:12 +0800

    组件通信-父传子-基础实现

--- react-basic/src/App.js
+++ react-basic/src/App.js
@@ -1,10 +1,18 @@
-// 项目的根组件
-// App -> index.js -> public/index.html(root)
+// 父传子
+// 1. 父组件传递数据 子组件标签身上绑定属性
+// 2. 子组件接收数据 props 参数
+
+function Son(props) {
+    // props: 对象里面包含了父组件传递过来的所有数据
+    console.log(props);
+    return <div>this is son {props.name}</div>
+}
 
 function App() {
+    const name = "this is app name";
     return (
         <div className="App">
-            this is App
+            <Son name={name}/>
         </div>
     );
 }
