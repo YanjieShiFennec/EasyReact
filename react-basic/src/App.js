fcc95aed587b0a5684512529a1e7346d9b59bbaf
yshi80 <yshi80@ford.com>
2024-09-30 21:29:49 +0800

    useEffect-清除副作用

--- react-basic/src/App.js
+++ react-basic/src/App.js
@@ -1,10 +1,26 @@
-// 项目的根组件
-// App -> index.js -> public/index.html(root)
+import {useEffect, useState} from "react";
+
+function Son() {
+    // 1. 渲染时开启一个定时器
+    useEffect(() => {
+        const timer = setInterval(() => {
+            console.log("定时器执行中...");
+        }, 1000);
+
+        return () => {
+            // 清除副作用(组件卸载时执行)
+            clearInterval(timer);
+        };
+    }, []);
+    return <div>this is son</div>;
+}
 
 function App() {
+    const [show, setShow] = useState(true);
     return (
         <div className="App">
-            this is App
+            {show && <Son/>}
+            <button onClick={() => setShow(false)}>卸载 Son 组件</button>
         </div>
     );
 }
