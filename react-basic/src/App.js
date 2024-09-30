c086637689001005fee09095e02be5d309d07350
yshi80 <yshi80@ford.com>
2024-09-30 21:10:55 +0800

    useEffect-概念理解和基础使用

--- react-basic/src/App.js
+++ react-basic/src/App.js
@@ -1,10 +1,28 @@
-// 项目的根组件
-// App -> index.js -> public/index.html(root)
+import {useEffect, useState} from "react";
+
+const URL = "http://geek.itheima.net/v1_0/channels";
 
 function App() {
+    // 创建一个状态数据
+    const [list, setList] = useState([]);
+    useEffect(() => {
+        // 组件渲染完毕之后
+        // 额外的操作 获取频道列表
+        async function getList() {
+            const res = await fetch(URL);
+            const jsonRes = await res.json();
+            console.log(jsonRes);
+            setList(jsonRes.data.channels);
+        }
+
+        getList();
+    }, []);
     return (
         <div className="App">
             this is App
+            <ul>
+                {list.map(item => <li key={item.id}>{item.name}</li>)}
+            </ul>
         </div>
     );
 }
