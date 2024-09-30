052d58859aa621c8e6e73a9f77d881648a7343c2
yshi80 <yshi80@ford.com>
2024-09-30 21:39:30 +0800

    自定义Hook实现

--- react-basic/src/App.js
+++ react-basic/src/App.js
@@ -1,10 +1,37 @@
-// 项目的根组件
-// App -> index.js -> public/index.html(root)
+// 封装自定义 Hook
+
+// 问题：布尔切换的逻辑 当前组件耦合在一起 不方便复用
+
+// 解决思路：自定义 Hook
+
+import {useState} from "react";
+
+function useToggle() {
+    // 可复用的逻辑代码
+    const [value, setValue] = useState(true);
+
+    const toggle = () => setValue(!value);
+
+    // 哪些状态和回调函数需要在其他组件中使用 return
+    return {
+        value,
+        toggle
+    };
+}
+
+// 封装自定义 Hook 通用思路
+// 1. 声明一个以 use 开头的函数
+// 2. 在函数体内封装可复用的逻辑（只要是可复用的逻辑）
+// 3. 把组件中用到的状态或回调 return 出去（以对象或者数组）
+// 4. 在哪个组件中要用到这个逻辑，就执行这个函数，解构出来状态和回调进行使用
 
 function App() {
+    const {value, toggle} = useToggle();
+
     return (
         <div className="App">
-            this is App
+            {value && <div>this is div</div>}
+            <button onClick={toggle}>toggle</button>
         </div>
     );
 }
