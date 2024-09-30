3f887d08e40c1b6fc894a2ba296c608019671a7b
yshi80 <yshi80@ford.com>
2024-09-30 21:44:00 +0800

    ReactHook使用规则说明

--- react-basic/src/App.js
+++ react-basic/src/App.js
@@ -1,7 +1,15 @@
-// 项目的根组件
-// App -> index.js -> public/index.html(root)
+// 1. 组件外使用 Hook
+// 2. if for 组件内部函数
+
+import {useState} from "react";
+
+// useState('')
 
 function App() {
+    if (Math.random() > 0.5) {
+        useState();
+    }
+
     return (
         <div className="App">
             this is App
