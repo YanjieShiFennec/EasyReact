834177141261aefaa07b05fb4e58d3ee2ed576ff
yshi80 <yshi80@ford.com>
2024-10-01 20:41:50 +0800

    案例优化-自定义评论item组件封装

--- react-basic/src/App.js
+++ react-basic/src/App.js
@@ -98,7 +98,47 @@ function useGetList() {
     return {
         commentList,
         setCommentList
-    }
+    };
+}
+
+// 封装 Item 组件
+function Item({item, onDel}) {
+    return (
+        <div className="reply-item">
+            {/* 头像 */}
+            <div className="root-reply-avatar">
+                <div className="bili-avatar">
+                    <img
+                        className="bili-avatar-img"
+                        alt=""
+                        src={item.user.avatar}
+                    />
+                </div>
+            </div>
+
+            <div className="content-wrap">
+                {/* 用户名 */}
+                <div className="user-info">
+                    <div className="user-name">{item.user.uname}</div>
+                </div>
+                {/* 评论内容 */}
+                <div className="root-reply">
+                    <span className="reply-content">{item.content}</span>
+                    <div className="reply-info">
+                        {/* 评论时间 */}
+                        <span className="reply-time">{item.ctime}</span>
+                        {/* 评论数量 */}
+                        <span className="reply-time">点赞数:{item.like}</span>
+                        {/*{条件：user.id === item.user.id}*/}
+                        {user.uid === item.user.uid &&
+                            <span className="delete-btn" onClick={() => onDel(item.rpid)}>
+                                                删除
+                                            </span>}
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 const App = () => {
@@ -207,42 +247,7 @@ const App = () => {
                 {/* 评论列表 */}
                 <div className="reply-list">
                     {/* 评论项 */}
-                    {commentList.map(item => (
-                        <div key={item.rpid} className="reply-item">
-                            {/* 头像 */}
-                            <div className="root-reply-avatar">
-                                <div className="bili-avatar">
-                                    <img
-                                        className="bili-avatar-img"
-                                        alt=""
-                                        src={item.user.avatar}
-                                    />
-                                </div>
-                            </div>
-
-                            <div className="content-wrap">
-                                {/* 用户名 */}
-                                <div className="user-info">
-                                    <div className="user-name">{item.user.uname}</div>
-                                </div>
-                                {/* 评论内容 */}
-                                <div className="root-reply">
-                                    <span className="reply-content">{item.content}</span>
-                                    <div className="reply-info">
-                                        {/* 评论时间 */}
-                                        <span className="reply-time">{item.ctime}</span>
-                                        {/* 评论数量 */}
-                                        <span className="reply-time">点赞数:{item.like}</span>
-                                        {/*{条件：user.id === item.user.id}*/}
-                                        {user.uid === item.user.uid &&
-                                            <span className="delete-btn" onClick={() => handleDel(item.rpid)}>
-                                                删除
-                                            </span>}
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    ))}
+                    {commentList.map(item => <Item key={item.rpid} item={item} onDel={handleDel}/>)}
                 </div>
             </div>
         </div>
