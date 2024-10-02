a42ce02246af39435e3081dd9d08edaeb0364fc6
yshi80 <yshi80@ford.com>
2024-10-02 11:03:15 +0800

    Redux与React-实现counter

--- react-redux-pro/src/App.js
+++ react-redux-pro/src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import {useDispatch, useSelector} from "react-redux";
+// 导入 actionCreater
+import {increment, decrement} from './store/modules/counterStore'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const {count} = useSelector(state => state.counter);
+    const dispatch = useDispatch();
+    return (
+        <div className="App">
+            <button onClick={() => dispatch(decrement())}>-</button>
+            {count}
+            <button onClick={() => dispatch(increment())}>+</button>
+        </div>
+    );
 }
 
 export default App;
