f38196bba1a28ee8a57ff91730b94dbf095c97ac
yshi80 <yshi80@ford.com>
2024-10-02 21:33:36 +0800

    美团案例-添加购物车实现

--- redux-meituan/src/store/modules/takeaway.js
+++ redux-meituan/src/store/modules/takeaway.js
@@ -9,7 +9,9 @@ const foodsStore = createSlice({
         // 商品列表
         foodsList: [],
         // 菜单激活下标值
-        activeIndex: 0
+        activeIndex: 0,
+        // 购物车列表
+        cartList: []
     },
     reducers: {
         setFoodsList(state, action) {
@@ -18,12 +20,22 @@ const foodsStore = createSlice({
         // 更改 activeIndex
         changeActiveIndex(state, action) {
             state.activeIndex = action.payload;
+        },
+        // 添加购物车
+        addCart(state, action) {
+            // 是否添加过，以 action.payload.id 去 cartList 中匹配
+            const item = state.cartList.find(item => item.id === action.payload.id);
+            if (item) {
+                item.count++;
+            } else {
+                state.cartList.push(action.payload);
+            }
         }
     }
 });
 
 // 异步获取部分
-const {setFoodsList, changeActiveIndex} = foodsStore.actions;
+const {setFoodsList, changeActiveIndex, addCart} = foodsStore.actions;
 const fetchFoodsList = () => {
     return async (dispatch) => {
         // 编写异步逻辑
@@ -33,6 +45,6 @@ const fetchFoodsList = () => {
     };
 }
 
-export {fetchFoodsList, changeActiveIndex};
+export {fetchFoodsList, changeActiveIndex, addCart};
 const reducer = foodsStore.reducer;
 export default reducer;
