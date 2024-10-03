26008ecf3f294fd718c9d27b168a22a395029be0
yshi80 <yshi80@ford.com>
2024-10-03 11:36:03 +0800

    ReactRouter-嵌套路由配置

---
+++ react-router-pro/src/page/Layout/index.js
@@ -0,0 +1,16 @@
+import {Link, Outlet} from "react-router-dom";
+
+const Layout = () => {
+    return (
+        <div>
+            一级路由 Layout 组件
+            <Link to='/board'>面板</Link>
+            <Link to='/about'>关于</Link>
+
+            {/*｛配置二级路由的出口｝*/}
+            <Outlet />
+        </div>
+    );
+};
+
+export default Layout;
--- react-router-pro/src/router/index.js
+++ react-router-pro/src/router/index.js
@@ -1,8 +1,25 @@
 import Login from "../page/Login";
 import Article from "../page/Article";
 import {createBrowserRouter} from "react-router-dom";
+import Layout from "../page/Layout";
+import Board from "../page/Board";
+import About from "../page/About";
 
 const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <Layout/>,
+        children: [
+            {
+                path: 'board',
+                element: <Board/>
+            },
+            {
+                path: 'about',
+                element: <About/>
+            }
+        ]
+    },
     {
         path: '/login',
         element: <Login/>
