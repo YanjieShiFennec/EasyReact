c5670b5029d636feeee9be4e3d4846186ca73fa0
yshi80 <yshi80@ford.com>
2024-10-03 11:58:19 +0800

    ReactRouter-两种路由模式

--- react-router-pro/src/router/index.js
+++ react-router-pro/src/router/index.js
@@ -1,12 +1,12 @@
 import Login from "../page/Login";
 import Article from "../page/Article";
-import {createBrowserRouter} from "react-router-dom";
+import {createBrowserRouter, createHashRouter} from "react-router-dom";
 import Layout from "../page/Layout";
 import Board from "../page/Board";
 import About from "../page/About";
 import NotFound from "../page/NotFound";
 
-const router = createBrowserRouter([
+const router = createHashRouter([
     {
         path: '/',
         element: <Layout/>,
