8af3f8f311c42b6f3a37bdfec1644156c24d8e19
yshi80 <yshi80@ford.com>
2024-10-03 10:50:01 +0800

    Revert "ReactRouter-快速开始"
    This reverts commit 634e3bb7c77e0dd43c5bfb32f076cbca25bc30f7.

--- react-router-pro/src/index.js
+++ react-router-pro/src/index.js
@@ -3,24 +3,11 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {createBrowserRouter, RouterProvider} from "react-router-dom";
-
-// 1. 创建 router 实例对象并且配置路由关系
-const router = createBrowserRouter([
-    {
-        path: '/login',
-        element: <div>登录页</div>
-    },
-    {
-        path: '/article',
-        element: <div>文章页</div>
-    }
-]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-        <RouterProvider router={router}></RouterProvider>
+        <App/>
     </React.StrictMode>
 );
 
