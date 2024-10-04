6b0e5a2ea4e412c48bed33350b01b79c70c80c14
yshi80 <yshi80@ford.com>
2024-10-04 20:49:07 +0800

    init

---
+++ react-jike/src/pages/Login/index.js
@@ -0,0 +1,12 @@
+import {Button} from "antd";
+
+const Login = () => {
+    return (
+        <div>
+            this is login
+            <Button type='primary'>test</Button>
+        </div>
+    );
+};
+
+export default Login;
