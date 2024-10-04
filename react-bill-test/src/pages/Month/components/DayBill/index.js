280446ac2109fd74bc159024adf7a5d197d05482
yshi80 <yshi80@ford.com>
2024-10-04 13:08:58 +0800

    月度账单-统计区域-单日账单列表渲染显示

--- react-bill-test/src/pages/Month/components/DayBill/index.js
+++ react-bill-test/src/pages/Month/components/DayBill/index.js
@@ -1,7 +1,7 @@
 import classNames from 'classnames'
 import './index.scss'
 import {useMemo} from 'react'
-// import {billTypeToName} from '@/contants/index'
+import {billTypeToName} from '@/constants/index'
 import {useState} from 'react'
 // import Icon from '@/components/Icon'
 
@@ -43,22 +43,20 @@ const DailyBill = ({date, billList}) => {
                 </div>
             </div>
             {/* 单日列表 */}
-            {/*<div className="billList" style={{display: visible ? 'block' : 'none'}}>*/}
-            {/*    {billList.map(item => {*/}
-            {/*        return (*/}
-            {/*            <div className="bill" key={item.id}>*/}
-            {/*                /!* 图标 *!/*/}
-            {/*                <Icon type={item.useFor}/>*/}
-            {/*                <div className="detail">*/}
-            {/*                    <div className="billType">{billTypeToName[item.useFor]}</div>*/}
-            {/*                </div>*/}
-            {/*                <div className={classNames('money', item.type)}>*/}
-            {/*                    {item.money.toFixed(2)}*/}
-            {/*                </div>*/}
-            {/*            </div>*/}
-            {/*        )*/}
-            {/*    })}*/}
-            {/*</div>*/}
+            <div className="billList">
+                {billList.map(item => {
+                    return (
+                        <div className="bill" key={item.id}>
+                            <div className="detail">
+                                <div className="billType">{billTypeToName[item.useFor]}</div>
+                            </div>
+                            <div className={classNames('money', item.type)}>
+                                {item.money.toFixed(2)}
+                            </div>
+                        </div>
+                    )
+                })}
+            </div>
         </div>
     )
 }
