a425207216a4d3fef6bc6c4d2f8580e33bb99347
yshi80 <yshi80@ford.com>
2024-10-04 14:37:12 +0800

    新增账单-支出和收入功能实现

--- react-bill-test/src/pages/New/index.js
+++ react-bill-test/src/pages/New/index.js
@@ -1,93 +1,99 @@
-import { Button, DatePicker, Input, NavBar } from 'antd-mobile'
+import {Button, DatePicker, Input, NavBar} from 'antd-mobile'
 import Icon from '@/components/Icon'
 import './index.scss'
 import classNames from 'classnames'
-import { billListData } from '@/constants'
-import { useNavigate } from 'react-router-dom'
+import {billListData} from '@/constants'
+import {useNavigate} from 'react-router-dom'
+import {useState} from "react";
 
 const New = () => {
-  const navigate = useNavigate()
-  return (
-    <div className="keepAccounts">
-      <NavBar className="nav" onBack={() => navigate(-1)}>
-        记一笔
-      </NavBar>
+    const navigate = useNavigate();
+    // 1. 准备一个控制收入支出的状态
+    const [billType, setBillType] = useState('pay'); // pay-支出 income-收入
 
-      <div className="header">
-        <div className="kaType">
-          <Button
-            shape="rounded"
-            className={classNames('selected')}
-          >
-            支出
-          </Button>
-          <Button
-            className={classNames('')}
-            shape="rounded"
-          >
-            收入
-          </Button>
-        </div>
+    return (
+        <div className="keepAccounts">
+            <NavBar className="nav" onBack={() => navigate(-1)}>
+                记一笔
+            </NavBar>
 
-        <div className="kaFormWrapper">
-          <div className="kaForm">
-            <div className="date">
-              <Icon type="calendar" className="icon" />
-              <span className="text">{'今天'}</span>
-              <DatePicker
-                className="kaDate"
-                title="记账日期"
-                max={new Date()}
-              />
-            </div>
-            <div className="kaInput">
-              <Input
-                className="input"
-                placeholder="0.00"
-                type="number"
-              />
-              <span className="iconYuan">¥</span>
-            </div>
-          </div>
-        </div>
-      </div>
-
-      <div className="kaTypeList">
-        {billListData['pay'].map(item => {
-          return (
-            <div className="kaType" key={item.type}>
-              <div className="title">{item.name}</div>
-              <div className="list">
-                {item.list.map(item => {
-                  return (
-                    <div
-                      className={classNames(
-                        'item',
-                        ''
-                      )}
-                      key={item.type}
-
+            <div className="header">
+                <div className="kaType">
+                    <Button
+                        className={classNames(billType === 'pay' ? 'selected' : '')}
+                        shape="rounded"
+                        onClick={() => setBillType('pay')}
                     >
-                      <div className="icon">
-                        <Icon type={item.type} />
-                      </div>
-                      <div className="text">{item.name}</div>
-                    </div>
-                  )
-                })}
-              </div>
-            </div>
-          )
-        })}
-      </div>
+                        支出
+                    </Button>
+                    <Button
+                        className={classNames(billType === 'income' ? 'selected' : '')}
+                        shape="rounded"
+                        onClick={() => setBillType('income')}
+                    >
+                        收入
+                    </Button>
+                </div>
 
-      <div className="btns">
-        <Button className="btn save">
-          保 存
-        </Button>
-      </div>
-    </div>
-  )
+                <div className="kaFormWrapper">
+                    <div className="kaForm">
+                        <div className="date">
+                            <Icon type="calendar" className="icon"/>
+                            <span className="text">{'今天'}</span>
+                            <DatePicker
+                                className="kaDate"
+                                title="记账日期"
+                                max={new Date()}
+                            />
+                        </div>
+                        <div className="kaInput">
+                            <Input
+                                className="input"
+                                placeholder="0.00"
+                                type="number"
+                            />
+                            <span className="iconYuan">¥</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div className="kaTypeList">
+                {billListData[billType].map(item => {
+                    return (
+                        <div className="kaType" key={item.type}>
+                            <div className="title">{item.name}</div>
+                            <div className="list">
+                                {item.list.map(item => {
+                                    return (
+                                        <div
+                                            className={classNames(
+                                                'item',
+                                                ''
+                                            )}
+                                            key={item.type}
+
+                                        >
+                                            <div className="icon">
+                                                <Icon type={item.type}/>
+                                            </div>
+                                            <div className="text">{item.name}</div>
+                                        </div>
+                                    )
+                                })}
+                            </div>
+                        </div>
+                    )
+                })}
+            </div>
+
+            <div className="btns">
+                <Button className="btn save">
+                    保 存
+                </Button>
+            </div>
+        </div>
+    )
 }
 
 export default New;
