68683a8413c3814388b8143e5db608e07f549283
yshi80 <yshi80@ford.com>
2024-10-05 14:59:33 +0800

    Axios请求头注入Token

--- react-jike/src/pages/Layout/index.js
+++ react-jike/src/pages/Layout/index.js
@@ -1,4 +1,13 @@
+// 测试 token 是否成功注入
+
+import {useEffect} from "react";
+import {request} from "@/utils";
+
 const Layout = () => {
+    useEffect(() => {
+        request.get('/user/profile');
+    }, []);
+
     return (
         <div>
             this is layout
