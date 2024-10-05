70058788c5047eda89952a256a579f0dd2876ad4
yshi80 <yshi80@ford.com>
2024-10-05 19:15:51 +0800

    Home-Echarts组件封装实现

---
+++ react-jike/src/pages/Home/components/BarChart.js
@@ -0,0 +1,44 @@
+// 柱状图组件
+
+// 1. 把功能代码都放到这个组件中
+// 2. 把可变的部分抽象成 prop 参数
+
+import {useEffect, useRef} from "react";
+import * as echarts from "echarts";
+
+const BarChart = ({title})=>{
+    const chartRef = useRef(null);
+    useEffect(() => {
+        // 保证 dom 可用才进行图表的渲染
+        // 1． 获取渲染图表的 dom 节点
+        const chartDom = chartRef.current;
+        // 2．图表初始化生成图表实例对象
+        const myChart = echarts.init(chartDom);
+        // 3．准备图表参数
+        const option = {
+            title: {
+                text: title
+            },
+            xAxis: {
+                type: 'category',
+                data: ['Vue', 'React', 'Angular']
+            },
+            yAxis: {
+                type: 'value'
+            },
+            series: [
+                {
+                    data: [10, 40, 70],
+                    type: 'bar'
+                }
+            ]
+        }
+        // 4. 使用图表参数完成图表的渲染
+        option && myChart.setOption(option);
+
+    }, []);
+
+    return <div ref={chartRef} style={{width: '500px', height: '400px'}}></div>;
+};
+
+export default BarChart;
