289ac1a408b3e0a1bced46e6cccce8cc6762ebcb
yshi80 <yshi80@ford.com>
2024-10-05 21:16:29 +0800

    文章封面-实现切换封面类型

--- react-jike/src/pages/Publish/index.js
+++ react-jike/src/pages/Publish/index.js
@@ -58,7 +58,13 @@ const Publish = () => {
     const onUploadChange = (value) => {
         // console.log("上传中", value);
         setImageList(value.fileList);
-    }
+    };
+
+    // 切换图片封面类型
+    const [imageType, setImageType] = useState(0);
+    const onTypeChange = (e) => {
+        setImageType(e.target.value);
+    };
 
     return (
         <div className="publish">
@@ -74,7 +80,7 @@ const Publish = () => {
                 <Form
                     labelCol={{span: 4}}
                     wrapperCol={{span: 16}}
-                    initialValues={{type: 1}}
+                    initialValues={{type: 0}}
                     onFinish={onFinish}
                 >
                     <Form.Item
@@ -96,7 +102,7 @@ const Publish = () => {
                     </Form.Item>
                     <Form.Item label="封面">
                         <Form.Item name="type">
-                            <Radio.Group>
+                            <Radio.Group onChange={onTypeChange}>
                                 <Radio value={1}>单图</Radio>
                                 <Radio value={3}>三图</Radio>
                                 <Radio value={0}>无图</Radio>
@@ -106,7 +112,7 @@ const Publish = () => {
                             listType: 决定选择文件框的外观样式
                             showUploadList: 控制显示上传列表
                         }*/}
-                        <Upload
+                        {imageType > 0 && <Upload
                             name="image"
                             listType="picture-card"
                             showUploadList
@@ -116,7 +122,8 @@ const Publish = () => {
                             <div style={{marginTop: 8}}>
                                 <PlusOutlined/>
                             </div>
-                        </Upload>
+                        </Upload>}
+
                     </Form.Item>
                     <Form.Item
                         label="内容"
