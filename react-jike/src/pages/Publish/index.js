ad90bde772f7af5bcec4b681a860d4faefcf692d
yshi80 <yshi80@ford.com>
2024-10-06 14:41:05 +0800

    编辑文章-回填基础数据

--- react-jike/src/pages/Publish/index.js
+++ react-jike/src/pages/Publish/index.js
@@ -10,13 +10,13 @@ import {
     Select, message
 } from 'antd'
 import {PlusOutlined} from '@ant-design/icons'
-import {Link} from 'react-router-dom'
+import {Link, useSearchParams} from 'react-router-dom'
 import './index.scss'
 
 import ReactQuill from 'react-quill'
 import 'react-quill/dist/quill.snow.css'
 import {useEffect, useState} from "react";
-import {createArticleAPI} from "@/apis/article";
+import {createArticleAPI, getArticleById} from "@/apis/article";
 import {useChannel} from "@/hooks/useChannel";
 
 
@@ -62,6 +62,22 @@ const Publish = () => {
         setImageType(e.target.value);
     };
 
+    // 回填数据
+    const [searchParams] = useSearchParams();
+    const articleId = searchParams.get('id');
+    // 获取实例
+    const [form] = Form.useForm();
+    useEffect(() => {
+        // 1. 通过id获取数据
+        async function getArticleDetail() {
+            const res = await getArticleById(articleId);
+            form.setFieldsValue(res.data);
+        }
+
+        getArticleDetail();
+        // 2. 调用实例方法完成回调
+    }, [articleId, form]);
+
     return (
         <div className="publish">
             <Card
@@ -78,6 +94,7 @@ const Publish = () => {
                     wrapperCol={{span: 16}}
                     initialValues={{type: 0}}
                     onFinish={onFinish}
+                    form={form}
                 >
                     <Form.Item
                         label="标题"
