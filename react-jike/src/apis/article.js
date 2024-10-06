53ea839d7498d89f86b45424b586ffd4710b1e26
yshi80 <yshi80@ford.com>
2024-10-06 15:27:20 +0800

    编辑文章-更新文章

--- react-jike/src/apis/article.js
+++ react-jike/src/apis/article.js
@@ -2,7 +2,7 @@
 
 import {request} from "@/utils";
 
-// 1. 获取频道列表
+// 获取频道列表
 export function getChannelAPI() {
     return request({
         url: '/channels',
@@ -10,7 +10,7 @@ export function getChannelAPI() {
     });
 }
 
-// 2. 提交文章表单
+// 提交文章表单
 export function createArticleAPI(data) {
     return request({
         url: '/mp/articles?draft=false',
@@ -19,7 +19,16 @@ export function createArticleAPI(data) {
     });
 }
 
-// 3. 获取文章列表
+// 更新文章表单
+export function updateArticleAPI(data) {
+    return request({
+        url: `/mp/articles/${data.id}?draft=false`,
+        method: 'PUT',
+        data: data
+    });
+}
+
+// 获取文章列表
 export function getArticleListAPI(params) {
     return request({
         url: '/mp/articles',
@@ -28,7 +37,7 @@ export function getArticleListAPI(params) {
     });
 }
 
-// 4. 删除文章
+// 删除文章
 export function delArticleAPI(id) {
     return request({
         url: `/mp/articles/${id}`,
@@ -36,7 +45,7 @@ export function delArticleAPI(id) {
     });
 }
 
-// 5. 获取文章详情
+// 获取文章详情
 export function getArticleById(id) {
     return request({
         url: `/mp/articles/${id}`
