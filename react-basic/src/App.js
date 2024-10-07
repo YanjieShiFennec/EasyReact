9ee56b2321d8a6149bcb6c56ecaf63cca3cc218b
yshi80 <yshi80@ford.com>
2024-10-07 10:05:02 +0800

    useReducer

--- react-basic/src/App.js
+++ react-basic/src/App.js
@@ -1,10 +1,35 @@
-// 项目的根组件
-// App -> index.js -> public/index.html(root)
+// useReducer
+import {useReducer} from "react";
+
+// 1. 定义 reducer 函数 根据不同的 action 返回不同的状态
+
+function reducer(state, action) {
+    switch (action.type) {
+        case 'INC':
+            return state + 1;
+        case 'DEC':
+            return state - 1;
+        case 'SET':
+            return action.payload;
+        default:
+            return state;
+    }
+}
+
+// 2. 组件中调用 useReducer(reducer, 0) => [state, dispatch]
+
+// 3. 调用 dispatch({type: 'INC'}) => 通知 reducer 产生一个新的状态 使用这个新状态更新 UI
 
 function App() {
+    const [state, dispatch] = useReducer(reducer, 0);
+
     return (
         <div className="App">
             this is App
+            <button onClick={() => dispatch({type: 'DEC'})}>-</button>
+            {state}
+            <button onClick={() => dispatch({type: 'INC'})}>+</button>
+            <button onClick={() => dispatch({type: 'SET', payload: 100})}>update</button>
         </div>
     );
 }
